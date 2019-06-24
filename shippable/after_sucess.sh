3388b537c227711c205213465bb601fce674cfaf
Mithun Kundu <mithun.kundu@hyland.com>
2019-06-24 17:49:27 +0530

    Fix

--- shippable/after_sucess.sh
+++ shippable/after_sucess.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 ###### Upload to DropBox using v2 api
-curl -X POST https://api.dropboxapi.com/2/files/create_folder_v2
-    --header "Authorization: Bearer $AUTH_TOKEN"
-    --header "Content-Type: application/json"
+curl -X POST https://api.dropboxapi.com/2/files/create_folder_v2 \
+    --header "Authorization: Bearer $AUTH_TOKEN" \
+    --header "Content-Type: application/json" \
     --data "{\"path\": \"/JniFiles/FreeRDP\",\"autorename\": false}"
 
 cd "client/Android/Studio/freeRDPCore/src/main"
@@ -15,7 +15,7 @@ do
   echo $item
   #filename=$(basename $item)
 curl -X POST https://content.dropboxapi.com/2/files/upload \
-  --header "Authorization: Bearer $AUTH_TOKEN" \
+    --header "Authorization: Bearer $AUTH_TOKEN" \
     --header "Dropbox-API-Arg: {\"path\": \"/JniFiles/FreeRDP/$APK_FILENAME\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" \
     --header "Content-Type: application/octet-stream" \
     --data-binary @$item
